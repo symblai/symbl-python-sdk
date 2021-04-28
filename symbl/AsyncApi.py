668d4cfbaa29420c34000048caa357298fe6e02d
Rishabh Chaturvedi <rishabh.chaturvedi@symbl.ai>
2021-04-29 00:21:30 +0530

    Async API working with decorator Contract

--- symbl/AsyncApi.py
+++ symbl/AsyncApi.py
@@ -14,14 +14,12 @@ class AsyncApi():
         self.api_client = api_client
         self.async_api_rest = async_api_rest(api_client)
 
-    def initialize_api_client(function):
+    def initialize_api_client(self, function=None):
         def wrapper(*args, **kw):
             credentials = None
-            self = args[0]
             
             if 'credentials' in kw:
                 credentials = kw['credentials']
-
             api_client = get_api_client(credentials)
             self.api_client = api_client
             self.async_api_rest = async_api_rest(api_client)
@@ -30,182 +28,226 @@ class AsyncApi():
         
         return wrapper
 
-    @initialize_api_client
-    def submit_text(self, credentials=None, text_payload : dict = None, wait: bool = False):
-        '''
-            Text payload to be analyzed
-            returns Job object
-        '''
+    def submit_text(self, function, credentials=None, text_payload : dict = None, wait: bool = False):
 
-        response = self.async_api_rest.add_text(body=text_payload)
-        print("Job with jobId " + response.job_id + " started!")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    Text payload to be analyzed
+                    returns Job object
+                '''
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                response = self.async_api_rest.add_text(body=text_payload)
+                print("Job with jobId " + response.job_id + " started!")
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-    @initialize_api_client
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
+
     def submit_audio(self, credentials=None, file_path:str = None, content_type:str='', wait:bool=True):
-        '''
-            audio files to be analyzed
-            returns Job object
-        '''
-        file = open(file_path, 'rb')
-        audio_file = file.read()
-        response = self.async_api_rest.add_audio(body=audio_file, content_type=content_type)
-        print("Job with jobId " + response.job_id + " started!")
+        
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    audio files to be analyzed
+                    returns Job object
+                '''
+                file = open(file_path, 'rb')
+                audio_file = file.read()
+                response = self.async_api_rest.add_audio(body=audio_file, content_type=content_type)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
 
-    @initialize_api_client
     def submit_video(self, credentials=None, file_path:str = None, content_type:str='', wait:bool=True):
-        '''
-            video files to be analyzed
-            returns Job object
-        '''
-        file = open(file_path, 'rb')
-        audio_file = file.read()
-        response = self.async_api_rest.add_audio(body=audio_file, content_type=content_type)
-        print("Job with jobId " + response.job_id + " started!")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    video files to be analyzed
+                    returns Job object
+                '''
+                file = open(file_path, 'rb')
+                audio_file = file.read()
+                response = self.async_api_rest.add_audio(body=audio_file, content_type=content_type)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    return function(Job(self.api_client, jobId=response.job_id, 
+                    conversationId=response.conversation_id, wait=wait))
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
+
+    def submit_audio_url(self, url : str=None, credentials=None, wait: bool = False):
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                
+                '''
+                    url of audio file to be analyzed
+                    returns Job object
+                '''
+                response = self.async_api_rest.add_audio_url(body={ 'url': url })
+                print("Job with jobId " + response.job_id + " started!")
 
 
-    @initialize_api_client
-    def submit_audio_url(self, credentials=None, url : str = None, wait: bool = False):
-        '''
-            url of audio file to be analyzed
-            returns Job object
-        '''
-        response = self.async_api_rest.add_audio_url(body={ 'url': url })
-        print("Job with jobId " + response.job_id + " started!")
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
+        
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
-
-    @initialize_api_client
     def submit_video_url(self, credentials=None, url : str = None, wait: bool = False):
-        '''
-            url of audio file to be analyzed
-            returns Job object
-        '''
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    url of audio file to be analyzed
+                    returns Job object
+                '''
 
-        response = self.async_api_rest.add_video_url(body={ 'url': url })
-        print("Job with jobId " + response.job_id + " started!")
+                response = self.async_api_rest.add_video_url(body={ 'url': url })
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
 
-    @initialize_api_client
     def append_text(self, credentials=None, text_payload : dict = None, conversation_id:str = None, wait: bool = False):
-        '''
-            Text payload to be appended
-            returns Job object
-        '''
-        if text_payload == None:
-            raise ValueError("Text Payload can not be None")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    Text payload to be appended
+                    returns Job object
+                '''
+                if text_payload == None:
+                    raise ValueError("Text Payload can not be None")
 
-        if conversation_id == None or len(conversation_id) == 0:
-            raise ValueError("Please enter a valid conversationId")
+                if conversation_id == None or len(conversation_id) == 0:
+                    raise ValueError("Please enter a valid conversationId")
 
-        response = self.async_api_rest.append_text(text_payload, conversation_id)
-        print("Job with jobId " + response.job_id + " started!")
+                response = self.async_api_rest.append_text(text_payload, conversation_id)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
 
-    @initialize_api_client
     def append_audio(self, credentials=None, file_path:str = None, conversation_id:str = None, content_type:str='', wait:bool=True):
-        '''
-            audio files to be appended
-            returns Job object
-        '''
-        file = open(file_path, 'rb')
-        audio_file = file.read()
-        response = self.async_api_rest.append_audio(body=audio_file, content_type=content_type, conversationId= conversation_id)
-        print("Job with jobId " + response.job_id + " started!")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    audio files to be appended
+                    returns Job object
+                '''
+                file = open(file_path, 'rb')
+                audio_file = file.read()
+                response = self.async_api_rest.append_audio(body=audio_file, content_type=content_type, conversationId= conversation_id)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
 
-    @initialize_api_client
     def append_video(self, credentials=None, file_path:str = None, conversation_id:str = None, content_type:str='', wait:bool=True):
-        '''
-            video files to be appended
-            returns Job object
-        '''
-        if conversation_id == None or len(conversation_id) == 0:
-            raise ValueError("Please enter a valid conversationId")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    video files to be appended
+                    returns Job object
+                '''
+                if conversation_id == None or len(conversation_id) == 0:
+                    raise ValueError("Please enter a valid conversationId")
 
-        file = open(file_path, 'rb')
-        video_file = file.read()
-        response = self.async_api_rest.appendVideo(body=video_file, content_type=content_type, conversationId=conversation_id)
-        print("Job with jobId " + response.job_id + " started!")
+                file = open(file_path, 'rb')
+                video_file = file.read()
+                response = self.async_api_rest.appendVideo(body=video_file, content_type=content_type, conversationId=conversation_id)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
   
-    @initialize_api_client  
     def append_audio_url(self, credentials=None, url : str = None, conversation_id:str = None, wait: bool = False):
-        '''
-            url of audio file to be appended
-            returns Job object
-        '''
-        response = self.async_api_rest.append_audio_url(body={ 'url': url }, conversation_id=conversation_id)
-        print("Job with jobId " + response.job_id + " started!")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    url of audio file to be appended
+                    returns Job object
+                '''
+                response = self.async_api_rest.append_audio_url(body={ 'url': url }, conversation_id=conversation_id)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
 
-    @initialize_api_client
     def append_video_url(self, credentials=None, url : str = None, conversation_id:str = None, wait: bool = False):
-        '''
-            url of video file to be appended
-            returns Job object
-        '''
-        response = self.async_api_rest.append_video_url(body={ 'url': url }, conversationId=conversation_id)
-        print("Job with jobId " + response.job_id + " started!")
+        def inner(function):
+            @self.initialize_api_client
+            def wrapper(*args, **kw):
+                '''
+                    url of video file to be appended
+                    returns Job object
+                '''
+                response = self.async_api_rest.append_video_url(body={ 'url': url }, conversationId=conversation_id)
+                print("Job with jobId " + response.job_id + " started!")
 
-        if not wait:
-            return Job(self.api_client, jobId=response.job_id, 
-            conversationId=response.conversation_id, wait=wait)
+                if not wait:
+                    job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                    return function(job)
 
-        job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
-        return job.getConversationId()
+                job = Job(self.api_client, jobId=response.job_id, conversationId=response.conversation_id, wait=wait)
+                return function(job.getConversationId())
+            return wrapper()
+        return inner
