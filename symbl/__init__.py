a7d2375e051dde4f2c2bf6ed03a1cc3072a672f5
Rishabh Chaturvedi <rishabh.chaturvedi@symbl.ai>
2021-05-10 02:10:47 +0530

    Updated init.py required for Streaming

--- symbl/__init__.py
+++ symbl/__init__.py
@@ -5,8 +5,12 @@ from symbl.async_api.Audio import Audio
 from symbl.async_api.Video import Video
 from symbl.async_api.Text import Text
 
+from symbl.Connection import Connection
+from symbl.streaming_api.StreamingConnection import StreamingConnection
+
 from symbl.conversations_api.ConversationsApi import ConversationsApi
 from symbl.telephony_api.TelephonyApi import TelephonyApi
+from symbl.streaming_api.StreamingApi import StreamingApi
 from symbl.jobs_api.Job import Job
 from symbl.jobs_api.JobStatus import JobStatus
 
@@ -15,3 +19,4 @@ Video = Video()
 Text = Text()
 Conversations = ConversationsApi()
 Telephony = TelephonyApi()
+Streaming = StreamingApi()
