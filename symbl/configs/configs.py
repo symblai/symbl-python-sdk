1943258571525d48c7e76401d393552a6aef9ae9
Rishabh Chaturvedi <rishabh.chaturvedi@symbl.ai>
2021-05-14 01:30:35 +0530

    packages added

---
+++ symbl/configs/configs.py
@@ -0,0 +1,3 @@
+X_API_KEY_HEADER = "x-api-key"
+SYMBL_WEBSOCKET_BASE_PATH = "wss://api.symbl.ai/session/subscribe/"
+SYMBL_STREAMING_API_FORMAT = "wss://api.symbl.ai/v1/realtime/insights/{}?access_token={}"
