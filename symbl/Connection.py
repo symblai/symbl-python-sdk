cf06500939ea12a92391fb588ca87afbab617a58
Rishabh Chaturvedi <rishabh.chaturvedi@symbl.ai>
2021-05-19 02:42:02 +0530

    Telephony final changes

--- symbl/Connection.py
+++ symbl/Connection.py
@@ -1,9 +1,8 @@
 from symbl.configs.configs import SYMBL_WEBSOCKET_BASE_PATH, X_API_KEY_HEADER
-from symbl.conversations_api.ConversationsApi import ConversationsApi
-import json
-import threading
-import websocket
 from symbl.AuthenticationToken import get_api_header
+import threading
+import json
+import websocket
 
 class Connection():
 
@@ -16,37 +15,40 @@ class Connection():
         self.eventUrl = eventUrl
         access_token = get_api_header(self.credentials).get(X_API_KEY_HEADER)
         self.header = "{}:{}".format(X_API_KEY_HEADER, access_token)
-        self.connection = websocket.WebSocket()
+        self.connection = None
 
-    def async_subscribe(self, eventCallbacks: dict):
-        print("Eshtablishing connection")
-        # print("Connecting to " + str( SYMBL_WEBSOCKET_BASE_PATH + self.connectionId) + " with api header ", self.header)
-        # print("Eshtablishing connection", self.connection)
-        self.connection.connect(url=SYMBL_WEBSOCKET_BASE_PATH + self.connectionId, header=[self.header])
-        print("Connection Eshtablished")
-        while True:
-            try:
-                data = self.connection.recv()
-                print(data)
-                if data != None and type(data) == str:  
-                    json_data = json.loads(data)
-                    if 'type' in json_data and json_data['type'] in eventCallbacks:
-                        eventCallbacks[json_data['type']](data) 
-            except Exception as error:
-                print(error)
-                break
+    def __on_message_handler(self, data, event_callbacks):
+        try:
+            if data != None and type(data) == str:  
+                json_data = json.loads(data)
+                if 'type' in json_data and json_data['type'] in event_callbacks:
+                    event_callbacks[json_data['type']](data) 
+        except Exception as error:
+            print(error)
 
-    def subscribe(self, eventCallbacks: dict):
-        thread = threading.Thread(target=self.async_subscribe, args=(eventCallbacks,))
-        thread.start()
+    def async_subscribe(self, event_callbacks: dict = {}):
+        if event_callbacks != None or event_callbacks != {}:
+            print("Eshtablishing connection")
+            self.connection = websocket.WebSocketApp(url=SYMBL_WEBSOCKET_BASE_PATH + self.connectionId, header=[self.header])
+            print("Connection Eshtablished")
 
+            self.connection.on_message = lambda this, data : self.__on_message_handler(data=data, event_callbacks=event_callbacks)
+            self.connection.on_error = lambda self, error : print(error)
 
-    def async_stop(self):
-        self.connection.connect(url=SYMBL_WEBSOCKET_BASE_PATH + self.connectionId, header=[self.header])
-        self.connection.send(payload={
-            'type': 'stop_request'
-        })
+            self.connection.run_forever()
+        else:
+            print("Can not subscribe to empty events")
+
+    def subscribe(self, event_callbacks: dict):
+        try:
+            thread = threading.Thread(target=self.async_subscribe, args=(event_callbacks,))
+            thread.start()
+        except (KeyboardInterrupt, SystemExit):
+            self.stop()
+            print("Exiting")
 
     def stop(self):
-        thread = threading.Thread(target=self.async_stop, args=())
-        thread.start()
+        self.connection.send(data=str({
+            'type': 'stop_request'
+        }))
+        self.connection.close()
