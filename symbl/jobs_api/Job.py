9c34cb0286932c9a29d119aabf96fb7a43cbe2fe
Rishabh Chaturvedi <rishabh.chaturvedi@symbl.ai>
2021-05-19 01:45:44 +0530

    Final changes for Async

--- symbl/jobs_api/Job.py
+++ symbl/jobs_api/Job.py
@@ -1,71 +1,82 @@
 from symbl.AuthenticationToken import get_api_client
 from symbl.jobs_api.JobStatus import JobStatus
-import threading
 from symbl_rest import JobsApi
 
-class Job():
+import threading
+import time
 
-    INTERVAL_TIME_IN_SECONDS = 5  ## in seconds
 
-    def __init__(self, jobId=None, conversationId:str=None, wait=True):
-
-        self.job_id = jobId
-        self.conversation_id = conversationId
-        self.success_func = None
-        self.error_func = None
-        self.job_status = JobStatus.IN_PROGRESS
-        self.wait = wait
-
-    def initialize_api_client(function):
-        def wrapper(*args, **kw):
-            credentials = None
-            self = args[0]
-            
+def initialize_api_client(function):
+    def wrapper(*args, **kw):
+        credentials = None
+        self = args[0]
+        
+        if not hasattr(self,'__jobs_api'):
             if 'credentials' in kw:
                 credentials = kw['credentials']
-
             api_client = get_api_client(credentials)
-            self.api_client = api_client
-            self.jobs_api = JobsApi(api_client)
 
-            function(*args, **kw)
-        
-        return wrapper
+            self.__jobs_api = JobsApi(api_client)
+
+        function(*args, **kw)
+    
+    return wrapper
+class Job():
+
+    __INTERVAL_TIME_IN_SECONDS = 5  ## in seconds
+
+    def __init__(self, job_id: str, conversation_id:str, wait=True):
+
+        self.__job_id = job_id
+        self.__conversation_id = conversation_id
+        self.__success_func = None
+        self.__jobs_api = JobsApi()
+        self.__error_func = None
+        self.__job_status = JobStatus.IN_PROGRESS
+        self.__wait = wait
     
     def getConversationId(self):
-        return self.conversation_id
+        return self.__conversation_id
     
     def getJobStatus(self):
-        return self.job_status.value
+        return self.__job_status.value
 
     def on_complete(self, func):
-        self.success_func = func
+        self.__success_func = func
         return self
 
     def on_error(self, func):
-        self.error_func = func
+        self.__error_func = func
         return self
     
+
     @initialize_api_client
-    def monitorJob(self, conversation, interval=None, wait=True, credentials=None):
+    def __fetch_current_job_status(self, credentials=None):
+        if self.__jobs_api != None:
+            response = self.__jobs_api.get_job_status(self.__job_id)
+            self.__job_status = JobStatus(response.status)
+        else:
+            raise ValueError("Job object not initialized correctly. Please contact administrator.")
+
+    def syncronous_monitor_job(self, conversation, interval=None, wait=True, credentials=None):
         if interval is None:
-            interval = self.INTERVAL_TIME_IN_SECONDS
+            interval = self.__INTERVAL_TIME_IN_SECONDS
         
-        response = self.jobs_api.get_job_status(self.job_id)
-        
-        self.job_status = JobStatus(response.status)
+        while self.__job_status != JobStatus.COMPLETED and self.__job_status != JobStatus.FAILED:
+            self.__fetch_current_job_status(credentials=credentials)
+            print("Fetching latest status of job {0}, current status is {1}".format(self.__job_id, self.__job_status.value))
+            time.sleep(interval)
 
-        print("Fetching latest status of job {0}, current status is {1}".format(self.job_id, response.status))
-        
-        if JobStatus(response.status) != JobStatus.COMPLETED and JobStatus(response.status) != JobStatus.FAILED:
-            timer = threading.Timer(interval, self.monitorJob, [conversation, interval, wait, credentials])
-            timer.start()
-            if wait == True:
-                timer.join()
-        
-        elif(JobStatus(response.status) == JobStatus.COMPLETED):
-            if self.success_func != None:
-                self.success_func(conversation)
+        if self.__job_status == JobStatus.COMPLETED and self.__success_func != None:
+            self.__success_func(conversation)
 
-        elif self.error_func != None:
-            self.error_func(conversation)
+        elif self.__error_func != None:
+            self.__error_func(conversation)
+
+    
+    def monitor_job(self, conversation, interval=None, wait=True, credentials=None):
+        if wait:
+            self.syncronous_monitor_job(conversation, interval, wait, credentials)
+        else:
+            thread = threading.Thread(target=self.syncronous_monitor_job, args=(conversation, interval, wait, credentials))
+            thread.start()
--- symbl/jobs_api/JobStatus.py
+++ symbl/jobs_api/JobStatus.py
@@ -5,3 +5,10 @@ class JobStatus(Enum):
     IN_PROGRESS = "in_progress"
     FAILED = "failed"
     COMPLETED = "completed"
+
+    def __eq__(self, other): 
+        if not isinstance(other, JobStatus):
+            # don't attempt to compare against unrelated types
+            raise ValueError("Can not compare JobStatus type object with {} type object".format(type(other)))
+
+        return self.value == other.value
