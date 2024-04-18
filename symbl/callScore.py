21a9177b6087d9d14114a5a09cbcfc70ce6ce863
Manoj Mali <manojmali@Manojs-MacBook-Pro-2.local>
2024-04-18 16:25:58 +0530

    test for get call score

--- symbl/callScore.py
+++ symbl/callScore.py
@@ -4,4 +4,4 @@ conversations = ConversationsApi();
 
 conversation_id=6750867925630976 # Update with the conversation Id of your conversation
 
-print(conversations.get_call_score_status(conversation_id))
+print(conversations.get_call_score(conversation_id))
